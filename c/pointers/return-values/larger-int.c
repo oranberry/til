56fbbc2df8673896e530f59a525758f9a9c52f90
Hyunjung Lee <leeleehj77@gmail.com>
2022-03-05 12:13:51 -0500

    Pointers as Return Values

---
+++ c/pointers/return-values/larger-int.c
@@ -0,0 +1,23 @@
+/* pointers to two integers, returns a pointer to whichever integer is larger */
+#include <stdio.h>
+int *max(int *a, int *b)
+{
+	if(*a > *b)
+		return a;
+	else
+		return b;
+}
+
+int main(){
+  int *p;
+  int i, j;
+  printf("Enter the two integer numbers: ");
+  scanf("%d%d", &i, &j);
+  
+  p = max(&i, &j);
+  printf("The larger integer is %d\n", *p);
+  return 0;
+}
+/* During the call of max, *a is an alias for i, while *b is an alias for j.
+If i has a larger value than j, max returns the address of i;
+otherwise, it returns the address of j. After the call, p points to either i or j.*/
