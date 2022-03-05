a2c8e79aa6694d10b97f6a98358b418f11b4dec5
Hyunjung Lee <leeleehj77@gmail.com>
2022-03-05 11:38:47 -0500

    Decompose into integral and fractional part

--- c/pointers/decompose-float/decompose.c
+++ c/pointers/decompose-float/decompose.c
@@ -1,18 +1,19 @@
-/* Decompose a floating-point number into its integral and fradtional part */
+/* Decompose a floating-point number into its integral and fractional part */
 #include <stdio.h>
 void decompose (double x, int *int_part, double *frac_part);
 
 int main(void)
 {
-    int n, i;
-    double d;
-
-    decompose(3.14159, &i, &d);
-    printf("Integer part: %d\nFractional part: %d\n", i, d);
-    return 0;
+  double n, d;
+  int i;
+  printf("Enter a floating-point number: ");
+  scanf("%lf", &n);
+  
+  decompose(n, &i, &d);
+  printf("Integer part: %d\nFractional part: %f\n", i, d);
+  
+  return 0;
 }
-
-
 
 void decompose(double x, int *int_part, double *frac_part)
 {
