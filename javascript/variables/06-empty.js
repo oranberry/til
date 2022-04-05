8c1864456609de61a42ff1f71a68602ef0cf26ec
Hyunjung Lee <leeleehj77@gmail.com>
2022-04-04 23:59:25 -0400

    JS variables basic

---
+++ javascript/variables/06-empty.js
@@ -0,0 +1,14 @@
+// null, undefined
+let variable;
+console.log(variable);
+
+variable = null;
+console.log(variable);
+
+let activeItem; // 아직 활성화된 아이템이 있는 지 없는 지 모르는 상태
+activeItem = null; // 활성화된 아이템이 없는 상태
+
+console.log(typeof 123); // number
+console.log(typeof '123'); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
