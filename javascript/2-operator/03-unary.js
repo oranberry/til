40c4dee9a8516ce32c33fd624a52770ee0b8018f
Hyunjung Lee <leeleehj77@gmail.com>
2022-04-11 22:53:14 -0400

    JavaScript Operator Basic Note

--- javascript/2-operator/03-unary.js
+++ javascript/2-operator/03-unary.js
@@ -4,24 +4,23 @@
 // ! (부정)
 let a = 5;
 a = -a; // -1 * 5
-console.log(a);
+console.log(a); // -5
 a = -a;
-console.log(a);
+console.log(a); // 5
 
 a = +a;
-console.log(a);
+console.log(a); // 5
 
 a = -a; // -5
 a = +a; // +(-5)
-console.log(a);
+console.log(a); // -5
 
 let boolean = true;
-console.log(boolean);
-console.log(!boolean);
-console.log(!!boolean);
+console.log(boolean); // true
+console.log(!boolean); // false
+console.log(!!boolean); // true
 
-// + 숫자가 아닌 타입들을 숫자로 변환하면 어떤 값이 나오는 지 확인 할 수 있음
-console.clear();
+// + : 숫자가 아닌 타입들을 숫자로 변환하면 어떤 값이 나오는 지 확인 할 수 있음
 console.log(+false); // 0
 console.log(+null); // 0
 console.log(+''); // 0
@@ -30,5 +29,6 @@ console.log(+'text'); // NaN
 console.log(+undefined); // NaN
 
 
-console.log(!!1); // ! 부정연산자
+console.log(!!1); // true
+// ! 부정연산자
 // !! 값을 boolean 타입으로 변환함
