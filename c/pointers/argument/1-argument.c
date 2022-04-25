9b05c6fcbc2f9aa86be8990047d34e10ff1f9b0a
Hyunjung Lee <leeleehj77@gmail.com>
2022-04-25 13:43:16 -0400

    Pointer as Argumennts Basic Notes

---
+++ c/pointers/argument/1-argument.c
@@ -0,0 +1,5 @@
+// 💫 Pointers as Arguments
+
+// arguments in calls of scanf are pointers:
+int i;
+scanf("%d", &i);
---
+++ c/pointers/argument/swap.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main()
+{
+  int x = 3, y = 4;
+  // call by reference
+  swap(&x, &y);
+  printf("The value of x is %d and y is %d\n", x, y);
+  /*** result: The value of x is 4 and y is 3 ***/
+  return 0;
+}
+
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+/*****************************************************************
+ * without pointers
+
+#include <stdio.h>
+void swap (int a, int b);
+
+int main()
+{
+  int x = 3, y = 4;
+  swap(x, y);
+  printf("The value of x is %d and y is %d\n", x, y);
+  *** result: The value of x is 3 and y is 4 (not working swap function) ***
+  return 0;
+}
+
+void swap(int a, int b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+******************************************************************/
+/*****************************************************************
+#include <stdio.h>
+void swap (int a, int b);
+
+int main()
+{
+  int x = 3, y = 4;
+  // call by value
+  swap(x, y); -> The value of x is 4 and y is 3
+  return 0;
+}
+
+void swap(int a, int b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+  printf("The value of x is %d and y is %d\n", a, b);
+}
+
+함수의 return 키워드를 사용해서 함수의 작업 결과 값을 함수를 호출한 쪽으로 전달할 수 있다.
+하지만 return은 한 번에 한 개의 값만 반환하고 두 개 이상의 값을 동시에 반환할 수 없기 때문에
+포인터를 사용하는 것이 더 효과적이다.
+******************************************************************/
