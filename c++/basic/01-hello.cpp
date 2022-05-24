a62ff15e63882e07478e46d2357f704dd0dc6998
Hyunjung Lee <leeleehj77@gmail.com>
2022-05-24 13:05:04 -0400

    C++ Basic Notes

--- c++/basic/01-hello.cpp
+++ c++/basic/01-hello.cpp
@@ -1,22 +1,23 @@
 // 간단한 C++ 프로그램
-#include <iostream>
+
 // 입출력을 포함한 많은 기본 기능은 표준 라이브러리(standard library)에 속해 있다.
 // 표준 라이브러리의 기능은 반드시 명시적으로 요청하여 사용해야 하는 데, 이를 위해 #include 지시문(directive)를 사용하여 요청한다.
 // iostream은 standard input/output streams library로 표준 라이브러리 중에서 순차적 또는 연속적인 입출력을 지원한다.
-// ex. cin, cout, cerr, clog, etc...
+// ex. cin, cout, ...
+#include <iostream>
 
 // 모든 C++ 프로그램은 반드시 main 함수를 포함한다 (main함수를 호출해서 프로그램을 실행함)
-int main() // 반환 타입 int, 매개변수 없음
+int main() // 반환 타입 int, 매개변수 없음을 나타냄
 {
-    // 표준 라이브러리의 출력 연산자(output operator) <<는 왼쪽 우선 결합성(left-associative)을 지님
-    // std::로 시작하는 코드는 해당 이름이 std라는 namespace에 속해 있음을 나타냄
-    // (std::cout은 '네임스테이스 std 범위에 있는 cout이라는 이름'을 의미)
+    // 표준 라이브러리의 출력 연산자(output operator) <<는 왼쪽 우선 결합성(left-associative)을 지니고 있다.
+    // std::로 시작하는 코드는 해당 이름이 std라는 namespace에 속해 있음을 나타낸다.
+    // (std::cout은 '네임스페이스 std 범위에 있는 cout이라는 이름'을 의미)
     // endl은 줄 바꿈 역할을 한다.
     std::cout << "Hello, world!" << std::endl; // std::cout << "Hello, world!\n";
 
     // 반환문
     // 일반적으로 모든 함수는 적어도 하나의 반환문을 포함해야 하며, 함수 끝에 도달한 것이 함수의 종료를 의미하지는 않는다.
-    // 반환 값은 함수가 반환할 것이라고 명시한 타입과 일치해야 한다.
+    // 반환 값은 함수가 반환할 것이라고 명시한 타입과 일치해야 한다. (여기에서는 int)
     // 프로그램은 여러 개의 반환문이 있을 수 있는 데, 모든 반환문은 반드시 적합한 타입의 값을 반환해야 한다.
     return 0;
 }
--- c++/basic/greeting2.cpp
+++ c++/basic/greeting2.cpp
@@ -1,4 +1,10 @@
 // 이름을 입력받아 테두리로 묶인 인사말을 생성하는 프로그램
+// ********************
+// *                  *
+// * Hello, Hyunjung! *
+// *                  *
+// ********************
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,20 +13,27 @@ int main()
 {
     cout << "Please enter your first name: ";
     string name;
-    cin >> name;
+    cin >> name; // Hyunjung 입력
 
     // 출력 메시지 구성
     // ⚠️ const는 소멸할 때까지 값을 변경하지 않겠다는 약속. 정의할 때 반드시 초기화 해야 한다.
-    const string greeting = "Hello, " + name + "!";
+    // name의 값은 프로그램을 실행하기 전까지는 정해져있지 않으므로 상수가 아니다.
+    const string greeting = "Hello, " + name + "!"; // Hello, Hyunjung!
 
     // 두 번째 행과 네 번째 행
-    const string spaces(greeting.size(), ' '); // 문자 개수만큼 공백을 넣는 상수
-    // greeting.size()로 greeting의 문자 개수를 나타내는 정수를 얻을 수 있다.
-    // spaces는 정숫값과 문자값을 받아 정숫값만큼 횟수로 문자값을 복사해 문자열을 만든다.
-    const string second = "* " + spaces + " *";
+    // greeting의 문자 개수만큼 공백을 넣는 상수 spaces
+    // greeting이라는 객체는 size라는 구성 요소가 있다. 이 구성 요소는 함수이므로 값을 얻으려고 호출할 수 있다.
+    // size() 함수: return the size of the set container or number of elements
+    // 즉, greeting.size()로 greeting의 문자 개수를 나타내는 정수를 얻을 수 있다.
+    // 문자 개수만큼 문자값(여기서는 ' ' 공백)을 복사해 문자열을 만든다.
+    const string spaces(greeting.size(), ' ');
+    const string second = "* " + spaces + " *"; // 문자열끼리 결합 (concatenate)
+    // + 연산자는 피연산자의 타입에 따라 실행하는 연산이 다르다. ex. 3 + 4 -> 7
+    // 연산자가 다양한 타입의 피연산자에 따라 다른 의미로 동작하는 것을 연산자의 오버로드(overload)라고 한다.
 
     // 첫 번째 행과 다섯 번째 행
-    const string first(second.size(), '*'); // 문자의 개수만큼 *을 포함하는 상수
+    // 정숫값과 문자값을 받아 정숫값만큼의 횟수로 문자값을 복사해 문자열을 만든다.
+    const string first(second.size(), '*');
 
     // 모두 출력
     cout << endl;
